25721435ca02111199edd7dc055b27381aafb68c
BerliozLeChat <neoflash93@gmail.com>
2015-11-27 15:47:35 +0100

    doc Neutre

--- src/Bloc/Neutre.cpp
+++ src/Bloc/Neutre.cpp
@@ -1,11 +1,27 @@
+/*!
+* \file Neutre.cpp
+* \brief Fichier contenant l'implémentation de la classe Neutre
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #include "Neutre.hpp"
 
+//--------------------------------------------------
+/*!
+* \brief Constructeur
+* \param bloc le bloc à décorer
+*/
 Neutre::Neutre(Bloc* bloc):Decorator(bloc){
 	texture.loadFromFile("res/box_neutre.png");
 	bloc->getSprite()->setTexture(texture);
 	bloc->setVictoire(false);
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui modifie l'attribut Bloc.cassable
+*/
 void Neutre::setCassable(){
 	bloc->setCassable(false);
 }
--- src/Bloc/Neutre.hpp
+++ src/Bloc/Neutre.hpp
@@ -1,9 +1,20 @@
+/*!
+* \file Neutre.hpp
+* \brief Fichier contenant la déclaration de la classe Neutre
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #ifndef Neutre_H
 #define Neutre_H
 
 #include "Decorator.hpp"
 #include "Bloc.hpp"
 
+/*!
+* \class Neutre
+* \brief classe qui hérite de Decorator et définit la propriété neutre d'un bloc
+*/
 class Neutre : public Decorator {
 		
 	public :
