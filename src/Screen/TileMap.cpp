71de10af79bdf049b799bc40b72c762f6faeb4ad
BerliozLeChat <neoflash93@gmail.com>
2015-11-27 14:55:33 +0100

    fix encodage

--- src/Screen/TileMap.cpp
+++ src/Screen/TileMap.cpp
@@ -1,6 +1,6 @@
 /*!
 * \file TileMap.cpp
-* \brief Fichier contenant l'implémentation de la classe TileMap
+* \brief Fichier contenant l'implÃ©mentation de la classe TileMap
 * \author Samia Ennaji
 * \date 27.11.2015
 */
@@ -9,10 +9,10 @@
 
 //--------------------------------------------------
 /*!
-* \brief Méthode qui crée un niveau via un tileset
-* \param tileset le fichier image utilisé pour les textures
+* \brief MÃ©thode qui crÃ©e un niveau via un tileset
+* \param tileset le fichier image utilisÃ© pour les textures
 * \param tileSize la taille d'une tuile
-* \param tiles un tableau représentant la grille du niveau où chaque entier fait référence à une texture
+* \param tiles un tableau reprÃ©sentant la grille du niveau oÃ¹ chaque entier fait rÃ©fÃ©rence Ã  une texture
 * \param width la largeur de la grille
 * \param height la hauteur de la grille
 */
@@ -30,23 +30,23 @@ bool TileMap::load(const string& tileset, Vector2u tileSize, const int* tiles, u
         for (unsigned int i = 0; i < width; ++i)
             for (unsigned int j = 0; j < height; ++j)
             {
-                // on récupère le numéro de tuile courant
+                // on rÃ©cupÃ¨re le numÃ©ro de tuile courant
                 int tileNumber = tiles[i + j * width];
 
-                // on en déduit sa position dans la texture du tileset
+                // on en dÃ©duit sa position dans la texture du tileset
                 int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
                 int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
 
-                // on récupère un pointeur vers le quad à définir dans le tableau de vertex
+                // on rÃ©cupÃ¨re un pointeur vers le quad Ã  dÃ©finir dans le tableau de vertex
                 Vertex* quad = &m_vertices[(i + j * width) * 4];
 
-                // on définit ses quatre coins
+                // on dÃ©finit ses quatre coins
                 quad[0].position = Vector2f(i * tileSize.x, j * tileSize.y);
                 quad[1].position = Vector2f((i + 1) * tileSize.x, j * tileSize.y);
                 quad[2].position = Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
                 quad[3].position = Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
 
-                // on définit ses quatre coordonnées de texture
+                // on dÃ©finit ses quatre coordonnÃ©es de texture
                 quad[0].texCoords = Vector2f(tu * tileSize.x, tv * tileSize.y);
                 quad[1].texCoords = Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
                 quad[2].texCoords = Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
@@ -58,9 +58,9 @@ bool TileMap::load(const string& tileset, Vector2u tileSize, const int* tiles, u
 
 //--------------------------------------------------
 /*!
-* \brief Méthode qui affiche le niveau
+* \brief MÃ©thode qui affiche le niveau
 * \param target objet qui affiche le niveau
-* \param states défini les propriétés du niveau
+* \param states dÃ©fini les propriÃ©tÃ©s du niveau
 */
 void TileMap::draw(RenderTarget& target, RenderStates states) const
 {
