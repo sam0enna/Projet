53d3c8117afe6e1a42968bfb8aba35fe96f29f16
BerliozLeChat <neoflash93@gmail.com>
2015-11-27 15:52:41 +0100

    doc Cassable

--- src/Bloc/Cassable.cpp
+++ src/Bloc/Cassable.cpp
@@ -1,11 +1,27 @@
+/*!
+* \file Cassable.cpp
+* \brief Fichier contenant l'implémentation de la classe Cassable
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #include "Cassable.hpp"
 
+//--------------------------------------------------
+/*!
+* \brief Constructeur
+* \param bloc le bloc à décorer
+*/
 Cassable::Cassable(Bloc* bloc):Decorator(bloc){
 	texture.loadFromFile("res/box_cassable.png");
 	bloc->getSprite()->setTexture(texture);
 	bloc->setVictoire(false);
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui modifie l'attribut Bloc.cassable
+*/
 void Cassable::setCassable(){
 	bloc->setCassable(true);
 }
--- src/Bloc/Cassable.hpp
+++ src/Bloc/Cassable.hpp
@@ -1,9 +1,20 @@
+/*!
+* \file Cassable.hpp
+* \brief Fichier contenant la déclaration de la classe Cassable
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #ifndef CASSABLE_H
 #define CASSABLE_H
 
 #include "Decorator.hpp"
 #include "Bloc.hpp"
 
+/*!
+* \class Cassable
+* \brief classe qui hérite de Decorator et définit la propriété cassable d'un bloc
+*/
 class Cassable : public Decorator {
 		
 	public :
