c44582f0853a8759e84e7d8a7c8ed8ed47baff11
BerliozLeChat <neoflash93@gmail.com>
2015-11-27 15:49:56 +0100

    doc Exit

--- src/Bloc/Exit.cpp
+++ src/Bloc/Exit.cpp
@@ -1,11 +1,27 @@
+/*!
+* \file Exit.cpp
+* \brief Fichier contenant l'implémentation de la classe Exit
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #include "Exit.hpp"
 
+//--------------------------------------------------
+/*!
+* \brief Constructeur
+* \param bloc le bloc à décorer
+*/
 Exit::Exit(Bloc* bloc):Decorator(bloc){
 	texture.loadFromFile("res/exit.png");
 	bloc->getSprite()->setTexture(texture);
 	bloc->setVictoire(true);
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui modifie l'attribut Bloc.cassable
+*/
 void Exit::setCassable(){
 	bloc->setCassable(false);
 }
--- src/Bloc/Exit.hpp
+++ src/Bloc/Exit.hpp
@@ -1,9 +1,20 @@
+/*!
+* \file Exit.hpp
+* \brief Fichier contenant la déclaration de la classe Exit
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #ifndef EXIT_H
 #define EXIT_H
 
 #include "Decorator.hpp"
 #include "Bloc.hpp"
 
+/*!
+* \class Exit
+* \brief classe qui hérite de Decorator et définit la propriété exit d'un bloc
+*/
 class Exit : public Decorator {
 		
 	public :
