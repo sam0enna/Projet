a2bc1d1d663824aa6d4848639ace5c9ac268593b
BerliozLeChat <neoflash93@gmail.com>
2015-11-27 15:03:53 +0100

    doc Bloc

--- src/Bloc/Bloc.cpp
+++ src/Bloc/Bloc.cpp
@@ -1,6 +1,18 @@
+/*!
+* \file Bloc.cpp
+* \brief Fichier contenant l'implémentation de la classe Bloc
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #include "Bloc.hpp"
 
-//constructeur
+//--------------------------------------------------
+/*!
+* \brief Constructeur
+* \param x la coordonnée x du bloc
+* \param y la coordonnée y du bloc
+*/
 Bloc::Bloc(int x, int y){
 	sprite = new Sprite();
 	sprite->scale(0.32f,0.32f);
@@ -8,40 +20,70 @@ Bloc::Bloc(int x, int y){
 	sprite->setPosition( x, y);
 }
 
+//--------------------------------------------------
+/*!
+* \brief Destructeur
+*/
 Bloc::~Bloc(){
 	delete sprite;
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui retourne le booléen cassable
+*/
 bool Bloc::estCassable(){
 	return cassable;
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui modifie la valeur de cassable
+* \param casse la nouvelle valeur
+*/
 void Bloc::setCassable(bool casse){
 	cassable = casse;
 }
-
-Bloc* Bloc::quandCasse(){ // lorsqu'une une entité essaye de casser un bloc
-	delete sprite;
-}
  
+//--------------------------------------------------
+/*!
+* \brief Méthode qui affiche le bloc
+* \param window la fenêtre
+*/
 void Bloc::display(RenderWindow& window){ //pour afficher un bloc 
 	if(sprite != nullptr)
 		window.draw(*sprite);
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui retourne le sprite
+*/
 Sprite* Bloc::getSprite()
 {
 	return sprite;
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui retourne la position du sprite
+*/
 Vector2f Bloc::getPosition(){
 	return sprite->getPosition();
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui modifie la valeur de victoire
+*/
 void Bloc::setVictoire(bool v){
 	victoire = v;
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui retourne la valeur de victoire
+*/
 bool Bloc::estVictorieux(){
 	return victoire;
 }
--- src/Bloc/Bloc.hpp
+++ src/Bloc/Bloc.hpp
@@ -1,22 +1,32 @@
+/*!
+* \file Bloc.hpp
+* \brief Fichier contenant la déclaration de la classe Bloc
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #ifndef BLOC_H
 #define BLOC_H
 
 #include <SFML/Graphics.hpp>
 using namespace sf;
 
+/*!
+* \class Bloc
+* \brief classe qui définit le type Bloc
+*/
 class Bloc
 {
 	private: 
-		Sprite* sprite;
-		bool cassable;
-		bool victoire;
+		Sprite* sprite;//!< le sprite du bloc
+		bool cassable;//!< détermine si le bloc est cassable ou non
+		bool victoire;//!< détermine si le bloc met fin à la partie
 	
 	public:
 		Bloc (int x, int y);
 		~Bloc();
 		bool estCassable();
 		void setCassable(bool);
-		Bloc* quandCasse();
 		void display(RenderWindow& window);
 		Sprite* getSprite();
 		Vector2f getPosition();
