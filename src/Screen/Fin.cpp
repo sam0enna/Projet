86542e5e865716fa8e18d53e067066bf9493c7f0
BerliozLeChat <neoflash93@gmail.com>
2015-11-27 14:35:46 +0100

    doc Fin

--- src/Screen/Fin.cpp
+++ src/Screen/Fin.cpp
@@ -1,16 +1,35 @@
+/*!
+* \file Fin.cpp
+* \brief Fichier contenant l'implémentation de la classe Fin
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #include "Fin.hpp"
 
+//--------------------------------------------------
+/*!
+* \brief Constructeur
+*/
 Fin::Fin(bool victoire)
 {
 	type = FIN;
 	victoire_ = victoire;
 }
 
+//--------------------------------------------------
+/*!
+* \brief Destructeur
+*/
 Fin::~Fin()
 {
 	unload();
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui charge les composants
+*/
 void Fin::load()
 {
 	if(!font.loadFromFile("res/Smart_Kid.otf"))
@@ -26,13 +45,30 @@ void Fin::load()
 	play.setPosition(320,240);
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui décharge les composants
+*/
 void Fin::unload()
 {
-	
 }
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui gère les déplacements gauche droite
+*/
 void Fin::move(int x,int y){}
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui gère les déplacements haut
+*/
+void Fin::haut(){}
+
+//--------------------------------------------------
+/*!
+* \brief Méthode qui affiche les composants
+*/
 void Fin::draw(RenderWindow &window)
 {
 	window.draw(play);
--- src/Screen/Fin.hpp
+++ src/Screen/Fin.hpp
@@ -1,3 +1,10 @@
+/*!
+* \file Fin.hpp
+* \brief Fichier contenant la déclaration de la classe Fin
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #ifndef FIN_H
 #define FIN_H
 #include <iostream>
@@ -8,6 +15,10 @@
 
 #include "ScreenState.hpp"
 
+/*!
+* \class Fin
+* \brief classe qui définit l'écran de jeu Fin
+*/
 class Fin : public ScreenState 
 {
 	public :
@@ -16,12 +27,13 @@ class Fin : public ScreenState
 		void load();
 		void unload();
 		void move(int x,int y);
+		void haut();
 		void draw(RenderWindow &window);
 		
 	private :
-		bool victoire_;
-		sf::Font font;
-		sf::Text play;
+		bool victoire_;//!< la condition de victoire
+		sf::Font font;//!< la police
+		sf::Text play;//!< le texte à afficher
 
 };
 #endif //FIN_H
