90b045d15dbb47508d260a5ffdbe5646f13edc2b
BerliozLeChat <neoflash93@gmail.com>
2015-11-27 15:43:47 +0100

    doc Decorator

--- src/Bloc/Decorator.cpp
+++ src/Bloc/Decorator.cpp
@@ -1,9 +1,29 @@
+/*!
+* \file Decorator.cpp
+* \brief Fichier contenant l'implémentation de la classe Decorator
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #include "Decorator.hpp"
 
+//--------------------------------------------------
+/*!
+* \brief Constructeur
+* \param bloc le bloc à décorer
+*/
 Decorator::Decorator(Bloc* bloc){
 	this->bloc = bloc;
 }
 
+//--------------------------------------------------
+/*!
+* \brief Destructeur
+*/
 Decorator::~Decorator(){}
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui modifie l'attribut Bloc.cassable
+*/
 void Decorator::setCassable(){}
--- src/Bloc/Decorator.hpp
+++ src/Bloc/Decorator.hpp
@@ -1,19 +1,30 @@
+/*!
+* \file Decorator.hpp
+* \brief Fichier contenant la déclaration de la classe Decorator
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #ifndef DECORATOR_H
 #define DECORATOR_H
 
 #include <SFML/Graphics.hpp>
 #include "Bloc.hpp"
 
+/*!
+* \class Decorator
+* \brief classe qui définit le type Decorator
+*/
 class Decorator {
 	
 	protected :
-		Bloc* bloc;
+		Bloc* bloc;//!< le bloc décoré
 		
 	public :
 		Decorator(Bloc*);
 		~Decorator();
 		virtual void setCassable();
-		Texture texture;
+		Texture texture;//!< la texture du bloc
 };
 
 #endif
