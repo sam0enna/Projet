4d2e16b3c7c0584725e1a67d9f60379b34575217
BerliozLeChat <neoflash93@gmail.com>
2015-11-27 16:26:22 +0100

    doc VieNeutre

--- src/Entite/VieNeutre.cpp
+++ src/Entite/VieNeutre.cpp
@@ -1,9 +1,28 @@
+/*!
+* \file VieNeutre.cpp
+* \brief Fichier contenant l'implémentation de la classe VieNeutre
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #include "VieNeutre.hpp"
 
+//--------------------------------------------------
+/*!
+* \brief Constructeur
+*/
 VieNeutre::VieNeutre(){}
 
+//--------------------------------------------------
+/*!
+* \brief Destructeur
+*/
 VieNeutre::~VieNeutre(){}
 
+//--------------------------------------------------
+/*!
+* \brief Méthode qui définit l'action de VieNeutre (0 de vie)
+*/
 int VieNeutre::doAction(){
 	return 0;
 }
--- src/Entite/VieNeutre.hpp
+++ src/Entite/VieNeutre.hpp
@@ -1,3 +1,10 @@
+/*!
+* \file VieNeutre.hpp
+* \brief Fichier contenant la déclaration de la classe VieNeutre
+* \author Samia Ennaji
+* \date 27.11.2015
+*/
+
 #ifndef VIENEUTRE_H
 #define VIENEUTRE_H
 
@@ -7,6 +14,10 @@
 using namespace std;
 using namespace sf;
 
+/*!
+* \class VieNeutre
+* \brief classe qui définit le type VieNeutre
+*/
 class VieNeutre : public ActionBonus{
 			
 	public:
